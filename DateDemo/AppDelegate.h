84fd60131369321c818fae4cdee4e7ad0fe9284b
beziershu <soloyan@yeah.net>
2017-01-12 14:24:34 +0800

    Initial Commit

---
+++ DateDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DateDemo
+//
+//  Created by  舒柳洋 on 2017/1/12.
+//  Copyright © 2017年 Liuyang Shu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DateDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DateDemo
+//
+//  Created by  舒柳洋 on 2017/1/12.
+//  Copyright © 2017年 Liuyang Shu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
